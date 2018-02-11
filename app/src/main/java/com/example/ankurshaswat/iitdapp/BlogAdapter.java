41f0e0421ea1ffeecb4170d89e05ecbef5c62de3
Shashwat Shivam <ankurshaswat@yahoo.co.in>
2018-02-11 20:39:11 +0530

    Reorganized files and added List Adapter for Blog Posts

---
+++ app/src/main/java/com/example/ankurshaswat/iitdapp/BlogAdapter.java
@@ -0,0 +1,10 @@
+package com.example.ankurshaswat.iitdapp;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by ankurshaswat on 11/2/18.
+ */
+
+public class BlogAdapter extends ArrayAdapter {
+}
---
+++ app/src/main/java/com/example/ankurshaswat/iitdapp/BlogItem.java
@@ -0,0 +1,8 @@
+package com.example.ankurshaswat.iitdapp;
+
+/**
+ * Created by ankurshaswat on 11/2/18.
+ */
+
+public class BlogItem {
+}
