502efb649ed5083d90fa7ec62f81107da43964ab
Shashwat Shivam <ankurshaswat@yahoo.co.in>
2018-02-15 16:11:29 +0530

    Code Cleanup and Reorganized

--- app/src/main/java/com/example/ankurshaswat/iitdapp/BlogFragment.java
+++ app/src/main/java/com/example/ankurshaswat/iitdapp/BlogFragment.java
@@ -36,9 +36,6 @@ public class BlogFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blog, container, false);
     }
