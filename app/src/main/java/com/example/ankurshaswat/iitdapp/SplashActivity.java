5fb6f4e58b52dadcccf90e5fdfc8c150f3ba42e6
KungFuPanda22 <khetankunal@gmail.com>
2018-09-18 01:44:21 +0530

    added loading screen
    used lottie

---
+++ app/src/main/java/com/example/ankurshaswat/iitdapp/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.ankurshaswat.iitdapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread welcomeThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(3000);  //Delay of 10 seconds
+                } catch (Exception e) {
+
+                } finally {
+
+                    Intent i = new Intent(SplashActivity.this,
+                            NewsMenuActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
+    }
+}
