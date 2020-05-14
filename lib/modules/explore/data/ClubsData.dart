6d9b2f74355ec076e4796482f54af66506ed1a32
Pranjal Aggarwal <pranjal2041@gmail.com>
2020-05-15 02:21:43 +0530

    Add Animation to Lists

--- lib/modules/explore/data/ClubsData.dart
+++ lib/modules/explore/data/ClubsData.dart
@@ -3,5 +3,14 @@ import 'dart:collection';
 List<Map> clubsList = [
   {'name': 'Dev Club', 'motto': 'We Code Things'},
   {'name': 'Dev Club', 'motto': 'We Build Things'},
-  {'name': 'Dev Club', 'motto': 'We are Losers'}
+  {'name': 'Dev Club', 'motto': 'We are Losers'},
+  {'name': 'Dev Club', 'motto': 'We Code Things'},
+  {'name': 'Dev Club', 'motto': 'We Build Things'},
+  {'name': 'Dev Club', 'motto': 'We are Losers'},
+  {'name': 'Dev Club', 'motto': 'We Code Things'},
+  {'name': 'Dev Club', 'motto': 'We Build Things'},
+  {'name': 'Dev Club', 'motto': 'We are Losers'},
+  {'name': 'Dev Club', 'motto': 'We Code Things'},
+  {'name': 'Dev Club', 'motto': 'We Build Things'},
+  {'name': 'Dev Club', 'motto': 'We are Losers'},
 ];
--- lib/modules/explore/widgets/CustomAnimatedListView.dart
+++ lib/modules/explore/widgets/CustomAnimatedListView.dart
@@ -1,5 +1,6 @@
 import 'package:IITDAPP/modules/explore/widgets/ClubListCard.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 // ignore: must_be_immutable
 class CustomAnimatedListView extends StatefulWidget {
@@ -13,17 +14,30 @@ class CustomAnimatedListView extends StatefulWidget {
 class _CustomAnimatedListViewState extends State<CustomAnimatedListView> {
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
+    return AnimationLimiter(
+      child: ListView.separated(
         padding: const EdgeInsets.all(8),
         itemCount: widget.list.length,
-
         itemBuilder: (BuildContext context, int index) {
           var temp = widget.list[index];
           temp['idx'] = index;
           widget.Item.data = temp;
-          return widget.Item;
+          return AnimationConfiguration.staggeredList(
+            position: index,
+            duration: const Duration(milliseconds: 400),
+            child: SlideAnimation(
+              verticalOffset: 50.0,
+              child: FadeInAnimation(
+                child: FlipAnimation(
+                  flipAxis: FlipAxis.y,
+                  child: widget.Item,
+                ),
+              ),
+            ),
+          );
         },
-      separatorBuilder: (context, index) => Divider(),
+        separatorBuilder: (context, index) => Divider(),
+      ),
     );
   }
 }
