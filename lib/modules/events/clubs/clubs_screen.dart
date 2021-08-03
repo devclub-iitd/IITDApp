fcf75ead5a090a115d7cc4ba0966801862e572a7
Pranjal Aggarwal <pranjal2041@gmail.com>
2021-08-03 23:52:35 +0530

    Minor Bug Fix

--- lib/modules/events/clubs/clubs_screen.dart
+++ lib/modules/events/clubs/clubs_screen.dart
@@ -62,7 +62,7 @@ class ClubsScreenState extends State<ClubsScreen> {
     print('makeClubCardList received:::');
     clubs.forEach((f) => print(f.clubName));
     // ignore: prefer_collection_literals
-    var toReturn = [];
+    var toReturn = <ClubCard>[];
     for (var i = 0; i < clubs.length; i++) {
       toReturn.add(ClubCard(clubs[i], ValueKey(clubs[i].id)));
       print('adding + ${clubs[i].clubName}');
