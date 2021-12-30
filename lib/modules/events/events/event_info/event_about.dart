2e5efb9efd08466a3e5acdc3065dc4cecf00ebe3
wiseyy <77568830+wiseyy@users.noreply.github.com>
2021-12-30 23:38:41 +0530

    changes

--- lib/modules/events/events/event_info/event_about.dart
+++ lib/modules/events/events/event_info/event_about.dart
@@ -3,11 +3,21 @@ import 'package:provider/provider.dart';
 import 'package:IITDAPP/widgets/sectionTitle.dart';
 import 'package:flutter/material.dart';
 import '../event_class.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class EventAbout extends StatelessWidget {
   final Event _event;
 
   const EventAbout(this._event);
+
+  Future<void> _launch(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      print("no");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,18 +32,38 @@ class EventAbout extends StatelessWidget {
               title: 'ABOUT THE EVENT',
             ),
             SectionUnderLine(),
-            Container(
-              margin: EdgeInsets.only(bottom: 20),
-              child: Text(
-                _event.about,
-                style: TextStyle(
-                  color:
-                      Provider.of<ThemeModel>(context).theme.PRIMARY_TEXT_COLOR,
-                  fontWeight: FontWeight.w300,
-                  fontSize: 15,
-                  letterSpacing: 0.5,
+            Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 20),
+                  child: SelectableLinkify(
+                    onOpen: (url) => {_launch(url.url), print(url)},
+                    text: _event.about,
+                    style: TextStyle(
+                      color: Provider.of<ThemeModel>(context)
+                          .theme
+                          .PRIMARY_TEXT_COLOR,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 15,
+                      letterSpacing: 0.5,
+                    ),
+                  ),
                 ),
-              ),
+                // Container(
+                //   margin: EdgeInsets.only(bottom: 20),
+                //   child: SelectableText(
+                //     _event.about,
+                //     style: TextStyle(
+                //       color: Provider.of<ThemeModel>(context)
+                //           .theme
+                //           .PRIMARY_TEXT_COLOR,
+                //       fontWeight: FontWeight.w300,
+                //       fontSize: 15,
+                //       letterSpacing: 0.5,
+                //     ),
+                //   ),
+                // ),
+              ],
             ),
             Center(child: Image.network(_event.imageLink)),
           ],
